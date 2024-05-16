1f0b2fde33d7c712a442182c709363ea8f7ae545
MelinaClouet <melina.clouet@my-digital-school.org>
2024-05-16 12:25:28 +0200

    [ADD] screen register et buton inscription login

--- lib/screen.login.dart
+++ lib/screen.login.dart
@@ -1,9 +1,8 @@
+import 'package:chatbot_filrouge/screen.register.dart';
 import 'package:flutter/material.dart';
 
-
-
 class ScreenLogin extends StatefulWidget {
-  const ScreenLogin({super.key});
+  const ScreenLogin({Key? key}) : super(key: key);
 
   @override
   State<ScreenLogin> createState() => _ScreenLoginState();
@@ -14,10 +13,29 @@ class _ScreenLoginState extends State<ScreenLogin> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Connexion' , style: TextStyle(fontSize: 30, color: Color(0xFF9F5540),fontWeight: FontWeight.bold, ),),
+        title: const Text(
+          'Connexion',
+          style: TextStyle(
+            fontSize: 30,
+            color: Color(0xFF9F5540),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ScreenRegister()),
+                );
+              },
+              child: const Text('Inscription'),
+            ),
+          ],
+        ),
       ),
     );
   }
