7a44e3a6ea048f1530388bcebb668a403b6544a6
MelinaClouet <melina.clouet@my-digital-school.org>
2024-05-16 11:54:30 +0200

    [ADD] page login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:chatbot_filrouge/screen.login.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Chatbot Fil Rouge',
+      theme: ThemeData(
+        useMaterial3: true,
       ),
+      home: const ScreenLogin(),
     );
   }
 }
+
