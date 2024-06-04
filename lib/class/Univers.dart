2da60a7cf762f4845429657f31b7641167e402e5
Javor Emir <emir.j2002@gmail.com>
2024-06-04 16:40:58 +0200

    getallunivers

---
+++ lib/class/Univers.dart
@@ -0,0 +1,19 @@
+import 'dart:convert'; // Add this import
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Univers {
+  Future<List<dynamic>> getAllUnivers(String? token) async {
+    var url = Uri.parse('https://mds.sprw.dev/universes');
+    var response =
+        await http.get(url, headers: {'Authorization': 'Bearer $token'});
+
+    if (response.statusCode == 200) {
+      debugPrint('Univers: ${response.body}');
+      // Parse the JSON response into a list
+      return json.decode(response.body) as List<dynamic>;
+    } else {
+      throw Exception('Failed to load data univers');
+    }
+  }
+}
