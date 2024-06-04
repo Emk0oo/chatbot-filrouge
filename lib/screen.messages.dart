18995d6ac1ddafbdf3a8e6f5507fd75563fd9981
Javor Emir <emir.j2002@gmail.com>
2024-06-04 11:53:06 +0200

    create message screen

---
+++ lib/screen.messages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScreenMessages extends StatefulWidget {
+  const ScreenMessages({super.key});
+
+  @override
+  State<ScreenMessages> createState() => _ScreenMessagesState();
+}
+
+class _ScreenMessagesState extends State<ScreenMessages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
